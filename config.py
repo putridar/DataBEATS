804916502b85c85f661bebfe598e9849abaf0e25
putridar <putri1805@gmail.com>
2023-04-15 17:43:02 +0800

    Add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+SERVICE_ACCOUNT = ""
+cid1 = ""
+secret1 = ""
+cid2 = ""
+secret2 = ""
+cid3 = ""
+secret3 = ""
+cid4 = ""
+secret4 = ""
+
+CIDS = [cid1, cid2, cid3, cid4]
+SECRETS = [secret1, secret2, secret3, secret4]
